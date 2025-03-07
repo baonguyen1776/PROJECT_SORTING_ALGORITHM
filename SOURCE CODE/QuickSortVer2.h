1b75e704d8520efba8eeeef11109893f00d227a4
AduSkibidi <ntquan2409@clc.fitus.edu.vn>
2025-03-07 13:57:25 +0700

    Update QuickSortVer2.h

--- SOURCE CODE/QuickSortVer2.h
+++ SOURCE CODE/QuickSortVer2.h
@@ -3,8 +3,9 @@
 
 #include <iostream>
 #include <cmath>
-#include <time.h>
+#include <chrono>
 using namespace std;
+using namespace std::chrono;
 
 double quickSortVer2Time(int *a, int n);
 unsigned long long quickSortVer2Cmp(int *a, int n);
