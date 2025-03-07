757c9fcecb5691cbc7f267c743434a7e0fc1b7ee
AduSkibidi <ntquan2409@clc.fitus.edu.vn>
2025-03-07 13:56:58 +0700

    Update MergeSort.h

--- SOURCE CODE/MergeSort.h
+++ SOURCE CODE/MergeSort.h
@@ -2,9 +2,10 @@
 #define MERGESORT_H
 
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <cmath>
 using namespace std;
+using namespace std::chrono;
 
 double mergeSortTime(int *a, int n);
 unsigned long long mergeSortCmp(int *a, int n);
