e28fb414ccc0a4af20bf2a175253914b8c434c5b
AduSkibidi <ntquan2409@clc.fitus.edu.vn>
2025-03-07 13:58:26 +0700

    Update SelectionSort.h

--- SOURCE CODE/SelectionSort.h
+++ SOURCE CODE/SelectionSort.h
@@ -2,9 +2,10 @@
 #define SELECTIONSORT_H
 
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <cmath>
 using namespace std;
+using namespace std::chrono;
 
 double selectionSortTime(int *a, int n);
 unsigned long long selectionSortCmp(int *a, int n);
