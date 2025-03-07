945326fd5f201f7d10b3ee748edf9eda666a2e86
AduSkibidi <ntquan2409@clc.fitus.edu.vn>
2025-03-07 13:55:58 +0700

    Update HeapSort.h

--- SOURCE CODE/HeapSort.h
+++ SOURCE CODE/HeapSort.h
@@ -2,9 +2,10 @@
 #define HEAPSORT_H
 
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <cmath>
 using namespace std;
+using namespace std::chrono;
 
 double heapSortTime(int *a, int n);
 unsigned long long heapSortCmp(int *a, int n);
