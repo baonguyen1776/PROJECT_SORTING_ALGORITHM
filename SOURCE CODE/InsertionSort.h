f7fc5c8d8105f662e1f5d8ec0e10ead682e1568c
AduSkibidi <ntquan2409@clc.fitus.edu.vn>
2025-03-07 13:56:34 +0700

    Update InsertionSort.h

--- SOURCE CODE/InsertionSort.h
+++ SOURCE CODE/InsertionSort.h
@@ -2,10 +2,10 @@
 #define INSERTIONSORT_H
 
 #include <iostream>
-#include <time.h>
+#include <chrono>
 #include <cmath>
 using namespace std;
-
+using namespace std::chrono;
 double insertionSortTime(int *a, int n);
 unsigned long long insertionSortCmp(int *a, int n);
 
