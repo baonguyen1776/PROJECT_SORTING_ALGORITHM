944b353d7039373303cb08d1c866f95097121b6e
phn0801 <ngophong0801@gmail.com>
2025-03-10 23:44:56 +0700

    Update LibrarySort.h

--- SOURCE CODE/OFFICIAL SOURCE CODE/LibrarySort.h
+++ SOURCE CODE/OFFICIAL SOURCE CODE/LibrarySort.h
@@ -22,10 +22,32 @@ Record getRecord(int arr[], int n, void (*sortFunctionTime)(int [], int), void (
 void copyArray(int oldArr[], int newArr[], int n);
 
 //All function sort to count time
-
+void selectionSortTime(int a[], int n);
+void insertionSortTime(int a[], int n);
+void bubbleSortTime(int a[], int n);
+void binaryInsertionSortTime(int a[], int n);
+void shakerSortTime(int a[], int n);
+void shellSortTime(int a[], int n);
+void heapSortTime(int a[], int n);
+void quickSortVer2Time(int a[], int n);
+void mergeSortTime(int a[], int n);
+void radixSortTime(int arr[], int n);
+void countingSortTime(int a[], int n);
+void flashSortTime(int a[], int n);
 
 //All function sort to count comparision
-
+void selectionSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void insertionSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void bubbleSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void binaryInsertionSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void shakerSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void shellSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void heapSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void quickSortVer2Cmp(int *a, int n, unsigned long long &numberOfCmp);
+void mergeSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void radixSortCmp(int arr[], int n, unsigned long long &numberOfCmp);
+void countingSortCmp(int a[], int n, unsigned long long &numberOfCmp);
+void flashSortCmp(int *a, int n, unsigned long long &numberOfCmp);
 
 //All data generation from teacher
 void GenerateRandomData(int a[], int n);
