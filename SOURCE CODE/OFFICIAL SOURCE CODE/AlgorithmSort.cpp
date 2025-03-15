e690c8c25bcbf0201d96a0994cbc36d1e0109fe2
baonguyen1776 <baogiaphuongnguyen1234@gmail.com>
2025-03-15 14:50:31 +0700

    fixed

--- SOURCE CODE/OFFICIAL SOURCE CODE/AlgorithmSort.cpp
+++ SOURCE CODE/OFFICIAL SOURCE CODE/AlgorithmSort.cpp
@@ -77,8 +77,8 @@ void insertionSortCmp(int a[], int n, unsigned long long &numberOfCmp) {
 //3.Bubble sort
 
 void bubbleSortTime(int a[], int n) {
-    for (int i = 1; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
             if (a[j] > a[j + 1])
                 swap(a[j], a[j + 1]);
         }
