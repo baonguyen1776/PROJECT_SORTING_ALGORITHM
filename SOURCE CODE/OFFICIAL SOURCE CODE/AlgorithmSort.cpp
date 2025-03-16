68d28791c248674c1f80a3a35ef7f2cd3e7a5513
baonguyen1776 <baogiaphuongnguyen1234@gmail.com>
2025-03-16 14:36:39 +0700

    fix shaker sort

--- SOURCE CODE/OFFICIAL SOURCE CODE/AlgorithmSort.cpp
+++ SOURCE CODE/OFFICIAL SOURCE CODE/AlgorithmSort.cpp
@@ -140,14 +140,13 @@ void shakerSortTime(int a[], int n) {
     right = n - 1;
     k = 0;
     while(left < right) {
-        for( int i = left;i < right; i++)
+        for(int i = left; i < right; i++)
             if(a[i] > a[i + 1]) {
                 swap(a[i], a[i + 1]);
                 k = i;
             }
         right = k;
-
-        for( int i = right;i > left; i--)
+        for(int i = right; i > left; i--)
             if(a[i] < a[i - 1]) {
                 swap(a[i], a[i - 1]);
                 k = i;
@@ -162,14 +161,13 @@ void shakerSortCmp(int a[], int n, unsigned long long &numberOfCmp) {
     right = n - 1;
     k = 0;
     while(++numberOfCmp && left < right) {
-        for( int i = left;++numberOfCmp && i < right; i++)
+        for(int i = left; ++numberOfCmp && i < right; i++)
             if(++numberOfCmp && a[i] > a[i + 1]) {
                 swap(a[i], a[i + 1]);
                 k = i;
             }
         right = k;
-
-        for( int i = right;++numberOfCmp && i > left; i--)
+        for(int i = right;++numberOfCmp && i > left; i--)
             if(++numberOfCmp && a[i] < a[i - 1]) {
                 swap(a[i], a[i - 1]);
                 k = i;
